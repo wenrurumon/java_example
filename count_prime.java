2b520265e4b739d711e4796bd63bc729cee45218
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-16 11:06:54 -0500

    Create count_prime.java

---
+++ count_prime.java
@@ -0,0 +1,18 @@
+public class count_prime{
+	public static void main(String[] args){
+		int b, cout=0;
+		for(int i = 101; i<=200; i += 1){
+			//for i 
+			for(int j = 2; j<=Math.sqrt(i); j++){
+				b = i % j;
+				//System.out.println(i + " " + j + " " + b);
+				if(b==0){
+					cout += 1;
+					break;
+				}
+			}
+			System.out.println(i + " " + cout);
+		}
+	System.out.println(100 - cout);
+	}
+}
