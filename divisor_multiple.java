1dc87db7d031f3c5cf3769b8c30c238a3b750d63
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-16 17:24:15 -0500

    Update divisor_multiple.java

--- divisor_multiple.java
+++ divisor_multiple.java
@@ -5,20 +5,29 @@ public class divisor_multiple{
 	public static void main(String[] args){
 		int m, n, out1, out2;
 		Scanner s = new Scanner(System.in);
+		System.out.println("Input m");
 		m = s.nextInt();
+		System.out.println("Input n");
 		n = s.nextInt();
 		//Minimal Covenant
 		int min = m>n ? n : m;
+		boolean b = true;
 		for(int i = 2; i <= min; i++){
 			if( m%i+n%i == 0){
+				System.out.println("Minimal Covenant");
 				System.out.println(i);
+				b = false;
 				break;
 			}
+		}
+		if(b){
+			System.out.println("No Minimal Covenant");
 		}
 		//Maximum public multiple
 		int max = m>n ? m : n;
 		for(int i = max; i <= m*n; i++){
 			if( i%m + i%n == 0){
+				System.out.println("Maximum public multiple");
 				System.out.println(i);
 				break;
 			}
