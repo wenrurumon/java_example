3a05d8fa879adee252678bcfb89e7181a48010c6
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-19 15:35:49 -0500

    Update string_count.java

--- string_count.java
+++ string_count.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
+class printer{
+	public void printer(int x){
+		System.out.println(x);
+	}
+}
+
 public class string_count{
 	public static void main(String[] args){
-		int count1 = 0, count2 = 0;
+		int count1 = 0, count2 = 0, count3 = 0, count4 = 0;
+		printer p = new printer();
 		Scanner input = new Scanner(System.in);
 		System.out.println("input string: ");
 		String s = input.nextLine();
 		char[] ch = s.toCharArray();
-		//System.out.println(ch);
 		for(int i = 0; i<ch.length; i++){
 			char chi = ch[i];
 			//System.out.println(i + ", " + chi);
@@ -15,8 +21,15 @@ public class string_count{
 				count1 ++;
 			} else if(Character.isDigit(chi)){
 				count2 ++;
+			} else if(Character.isSpaceChar(chi)){
+				count3 ++;
+			} else {
+				count4 ++;
 			}
 		}
-		System.out.println("count1="+count1+" count2="+count2);
+		p.printer(count1);
+		p.printer(count2);
+		p.printer(count3);
+		p.printer(count4);
 	}
 }
