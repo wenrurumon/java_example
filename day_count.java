6baada6eaf5ecd0ca9d1158f34216e42b250df7c
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-20 15:35:56 -0500

    Create day_count.java

---
+++ day_count.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+class date{
+	int year;
+	int month;
+	int day;
+	int date;
+	int[] daysinmonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+	public void init(int x){
+		year = x/10000;
+		month = x/100-year*100;
+		day = x - year*10000 - month*100;
+		if(year%400==0||(year%4==0 && year%100!=0)){
+			daysinmonth[1] = 29;
+		}
+		//System.out.println(year+" "+month+" "+day);		
+	}
+	public int countday(){
+		int temp = 0;
+		for(int i = 1; i<month; i++){
+			temp += daysinmonth[i-1];
+		}
+		temp += day;
+		return temp;
+	}
+}
+
+public class day_count{
+	public static void main(String[] args){
+		date x = new date();
+		Scanner s = new Scanner(System.in);
+		System.out.println("inpurt date");
+		int date = s.nextInt();
+		x.init(date);
+		System.out.println(x.countday());
+	}
+}
