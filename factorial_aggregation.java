a3df1eab44d0f4b4eb9fd6b0a63fddab22c1e58c
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-20 22:00:59 -0500

    Create factorial_aggregation.java

---
+++ factorial_aggregation.java
@@ -0,0 +1,30 @@
+
+class X{
+	double x;
+	public void init(double x_){
+		x = x_;
+	}
+	public double xf(){
+		double f = 1;
+		for(double i = 1; i<=x; i++){
+			f = f*i;
+		}
+		return f;
+	}
+}
+
+public class factorial_aggregation{
+	public static void main(String[] args){
+		X x = new X();
+		double sum1=0, sum2=0;
+		for(double i = 1; i<=20; i++){
+			x.init(i);
+			double x1;
+			x1 = x.xf();
+			sum1 += x1;
+			sum2 += 1/x1;
+		}
+		System.out.println(sum1);
+		System.out.println(sum2);
+	}
+}
